091efa92110e239103a55bae7728dc17d594b07e
glenwojnar1.0@gmail.com <112520930+glwo@users.noreply.github.com>
2026-09-16 20:37:23 -0400

    Solved 1.3 and 1.4

---
+++ 001_Intro/001_fahr_cel.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// Excercise 1.3: Modify the temperature conversion function to print a heading above the table.
+
+// void
+// main()
+// {
+//   float fahr, celsius;
+//   float lower, upper, step;
+//
+//   printf("F    C\n");
+//   lower = 0;
+//   upper = 300;
+//   step = 20;
+//
+//   fahr = lower;
+//
+//   while (fahr <= upper){
+//
+//   celsius = (5.0/9.0) * (fahr - 32);
+//   printf("%3.0f %6.1f\n", fahr, celsius);
+//   fahr = fahr + step;
+// }
+// }
+
+
+
+// Excercise 1.4: Write a program to print the corresponding celsius to Fahrenhiet table
+
+void
+main()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+
+  printf("C    F\n");
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  celsius = lower;
+
+  while (celsius <= upper){
+
+  fahr = (9.0/5.0) * (celsius + 32);
+  printf("%3.0f %6.1f\n", celsius, fahr);
+  celsius = celsius + step;
+}
+}
+
