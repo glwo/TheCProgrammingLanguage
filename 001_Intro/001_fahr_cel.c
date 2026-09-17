96ea5afc0ba565680b5562ad69108898163d14df
glenwojnar1.0@gmail.com <112520930+glwo@users.noreply.github.com>
2026-09-16 20:57:00 -0400

    Completed excer 1.5

--- 001_Intro/001_fahr_cel.c
+++ 001_Intro/001_fahr_cel.c
@@ -27,21 +27,34 @@
 // Excercise 1.4: Write a program to print the corresponding celsius to
 // Fahrenhiet table
 
+// void main() {
+//   float fahr, celsius;
+//   float lower, upper, step;
+//
+//   printf("C    F\n");
+//   lower = 0;
+//   upper = 300;
+//   step = 20;
+//
+//   celsius = lower;
+//
+//   while (celsius <= upper) {
+//
+//     fahr = (9.0 / 5.0) * (celsius + 32);
+//     printf("%3.0f %6.1f\n", celsius, fahr);
+//     celsius = celsius + step;
+//   }
+// }
+
+// Excercise 1.5: Modify the temperature conversion program to print the table
+// in reverse order, that is, from 300 degrees to 0
 void main() {
-  float fahr, celsius;
-  float lower, upper, step;
 
-  printf("C    F\n");
-  lower = 0;
-  upper = 300;
-  step = 20;
+  int fahr;
+  double celsius;
 
-  celsius = lower;
-
-  while (celsius <= upper) {
-
-    fahr = (9.0 / 5.0) * (celsius + 32);
-    printf("%3.0f %6.1f\n", celsius, fahr);
-    celsius = celsius + step;
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    celsius = (5.0 / 9.0) * (fahr - 32);
+    printf("%3.0i %6.1f\n", fahr, celsius);
   }
 }
