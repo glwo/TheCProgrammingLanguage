8489531e0e9dfafa7bf1010cb4d170d18d8f3ab7
glenwojnar1.0@gmail.com <112520930+glwo@users.noreply.github.com>
2026-09-17 21:30:44 -0400

    Finished 1.6

---
+++ 001_Intro/1.6_get_char.c
@@ -0,0 +1,13 @@
+// Excercise 1.6: Verify that the expression getchar() != EOF is 0 or 1
+#include <stdbool.h>
+#include <stdio.h>
+
+// int main() {
+//   bool res;
+//   res = 1 == (getchar() != EOF);
+//   printf("%d\n", res);
+// }
+
+// Excercise 1.7: Write a program to print the value of EOF.
+
+int main() { printf("%d\n", EOF); }
